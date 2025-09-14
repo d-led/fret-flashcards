4dfe8c049dac19c1bfb27640ff4f4456334afcbc
Dmitry Ledentsov <dmlled@yahoo.com>
2025-09-14 13:56:05 +0200

    convert cypress config to ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,28 +1,29 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+import http from "http";
+import path from "path";
+import sirv from "sirv";
+import { defineConfig } from "cypress";
 
 // Generate a random port between 3000 and 9999
 const port = Math.floor(Math.random() * 7000) + 3000;
 
-module.exports = {
+export default defineConfig({
   reporter: "mocha-junit-reporter",
   reporterOptions: {
-    mochaFile: "test-results/junit-[hash].xml"
+    mochaFile: "test-results/junit-[hash].xml",
   },
   e2e: {
     setupNodeEvents(on, config) {
-      let server;
+      // Reference 'on' to satisfy strict compiler settings (it's unused by this config)
+      void on;
+      let server: http.Server;
 
-      // Start the server immediately on random port and serve `src/dist` statically
-      const sirv = require("sirv");
+      // Start the server immediately on random port and serve `dist` statically
       const distRoot = path.join(__dirname, "dist");
       const serve = sirv(distRoot, { dev: false, single: true });
 
       server = http.createServer((req, res) => {
         // Delegate static file serving to sirv (single: true will serve index.html for SPA)
         serve(req, res, () => {
-          // If sirv didn't handle the request, return 404
           res.writeHead(404, { "Content-Type": "text/plain" });
           res.end("Not found");
         });
@@ -30,20 +31,19 @@ module.exports = {
 
       server
         .listen(port, () => {
+          // eslint-disable-next-line no-console
           console.log(`Test server running on http://localhost:${port}`);
         })
-        .on("error", (err) => {
+        .on("error", (err: NodeJS.ErrnoException & { code?: string }) => {
           if (err.code === "EADDRINUSE") {
+            // eslint-disable-next-line no-console
             console.log(`Port ${port} in use, trying ${port + 1}...`);
             server.listen(port + 1, () => {
+              // eslint-disable-next-line no-console
               console.log(`Test server running on http://localhost:${port + 1}`);
             });
           }
         });
-
-      on("task", {
-        // stopServer task removed as server now runs for entire session
-      });
 
       // Set the port in Cypress environment
       config.env = { ...config.env, port };
@@ -51,4 +51,4 @@ module.exports = {
       return config;
     },
   },
-};
+});
