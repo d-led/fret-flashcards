5fc9f747a09afe6697aff31ceb84193873af227e
Dmitry Ledentsov <dmlled@yahoo.com>
2025-09-16 20:25:37 +0200

    npm run prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     https: {
-      cert: '/tmp/fret-cert.pem',
-      key: '/tmp/fret-cert-key.pem'
+      cert: "/tmp/fret-cert.pem",
+      key: "/tmp/fret-cert-key.pem",
     },
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
   preview: {
     https: {
-      cert: '/tmp/fret-cert.pem',
-      key: '/tmp/fret-cert-key.pem'
+      cert: "/tmp/fret-cert.pem",
+      key: "/tmp/fret-cert-key.pem",
     },
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+  },
+});
